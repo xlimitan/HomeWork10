bd8421b7666599864e694f04724ec1472362cd61
xlimitan <xlimitan@yandex.ru>
2023-02-27 14:48:33 +0300

    Задача № 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1 () {
@@ -20,5 +21,11 @@ public class Main {
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - "+fullName);
     }
 }
